4e5c878d5e4f6b89ba2575729fa0378a5d3bbb09
The Lxx CLoUD <cloudgithuuhuh@outlook.com>
2026-08-14 23:46:02 +0330

    Add files via upload

--- icmp/handler.go
+++ icmp/handler.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/The-Lxx-CLoUD/CLoUD-ICMP"
+	"https://github.com/The-Lxx-CLoUD/CLoUD-ICMP"
 )
 
 func toASCII(data []byte) string {
--- main.go
+++ main.go
@@ -14,8 +14,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/The-Lxx-CLoUD/CLoUD-ICMP/crypto"
-	"github.com/The-Lxx-CLoUD/CLoUD-ICMP/icmp"
+	"https://github.com/The-Lxx-CLoUD/CLoUD-ICMP/crypto"
+	"https://github.com/The-Lxx-CLoUD/CLoUD-ICMP/icmp"
 )
 
 const (
